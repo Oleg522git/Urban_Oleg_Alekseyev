2c7f65d121806660cc440e5785073b0202b2a258
олег <9222194015@mail.ru>
2024-09-14 12:04:14 +0500

    Практическое задание

--- module_2_4.py
+++ module_2_4.py
@@ -1,6 +1,3 @@
-#from idlelib.configdialog import is_int
-from idlelib.configdialog import is_int
-
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, '11' , 13 , 'hg']
 primes = []
 not_primes = []
